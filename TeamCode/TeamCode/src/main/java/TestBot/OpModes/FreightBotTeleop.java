244d7573e4ff461a146d14cbd756eb02e77b6f3b
claireharrington <55068854+claireharrington@users.noreply.github.com>
2021-10-17 19:01:13 -0700

    changes as of 10.17

--- TeamCode/TeamCode/src/main/java/TestBot/OpModes/FreightBotTeleop.java
+++ TeamCode/TeamCode/src/main/java/TestBot/OpModes/FreightBotTeleop.java
@@ -3,13 +3,14 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 import TestBot.Mechanisms.FreightBotInfo;
 import TestBot.Mechanisms.MecanumDriveBase;
 
-@TeleOp
+@TeleOp(name = "FreightBotTeleop")
 public class FreightBotTeleop extends OpMode {
     FreightBotInfo robot = new FreightBotInfo();
 
@@ -20,7 +21,7 @@ public class FreightBotTeleop extends OpMode {
 
     @Override
     public void loop() {
-
+        telemetry.addData("some numbrs", robot.getTicksPerRev());
         telemetry.addData("Our Heading", robot.getHeading(AngleUnit.DEGREES));
         telemetry.addData("front left Motor Rotations", robot.getFLMotorRotations());
         telemetry.addData("front right Motor Rotations", robot.getFRMotorRotations());
@@ -31,11 +32,22 @@ public class FreightBotTeleop extends OpMode {
         robot.mecanumDrive(gamepad1.right_stick_y, -gamepad1.right_stick_x, -gamepad1.left_stick_x);
 
         //ducky mover
-        if(gamepad1.a){
-            robot.duckyMover.setPower(0.5);
+        if(gamepad1.a) {
+            robot.duckyMover.setPower(-1);
+        }else if(gamepad1.b){
+            robot.duckyMover.setPower(1);
+        } else {
+            robot.duckyMover.setPower(0);
         }
 
-
+        //alistair's lifting module
+        if (gamepad1.x) {
+            robot.moduleA.setPower(1.0);
+        } else if (gamepad1.y){
+            robot.moduleA.setPower(-1.0);
+        } else {
+            robot.moduleA.setPower(0);
+        }
     }
 }
 
