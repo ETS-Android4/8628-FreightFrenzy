183d71c7fe4f4fcdb77bc755e21ac91989873b51
claireharrington <55068854+claireharrington@users.noreply.github.com>
2022-02-04 20:39:13 -0800

    update before state

--- TeamCode/TeamCode/src/main/java/TestBot/OpModes/FreightBotTeleop.java
+++ TeamCode/TeamCode/src/main/java/TestBot/OpModes/FreightBotTeleop.java
@@ -25,37 +25,105 @@ public class FreightBotTeleop extends OpMode {
         robot.init(hardwareMap);
     }
 
+
     @Override
     public void loop() {
         telemetry.addData("Our Heading", robot.getHeading(AngleUnit.DEGREES));
         telemetry.addData("XRAIL POSITION", robot.liftMotor.getCurrentPosition());
 
 
+        telemetry.addData("left state", robot.digitalTouchLeft.getState());
+        telemetry.addData("right state", robot.digitalTouchRight.getState());
 
-        //mecanum drive
-        robot.mecanumDrive(gamepad1.right_stick_y, -gamepad1.right_stick_x, -gamepad1.left_stick_x);
-
+        //mecanum drive w/ precision mode
+        if (gamepad1.left_bumper) {
+            robot.mecanumDrive(0.5 * gamepad1.right_stick_y, -0.5 * gamepad1.right_stick_x, -0.5 * gamepad1.left_stick_x);
+        } else if (gamepad1.right_bumper) {
+            robot.mecanumDrive(0.25 * gamepad1.right_stick_y, -0.25 * gamepad1.right_stick_x, -0.25 * gamepad1.left_stick_x);
+        } else {
+            robot.mecanumDrive(gamepad1.right_stick_y, -gamepad1.right_stick_x, -gamepad1.left_stick_x);
+        }
         //start
 
 
-        //GAMEPAD 1 - JONAH
         // lift servo (BOX)
-        if (gamepad1.x) {
+        if (gamepad2.y) {
             robot.liftServo(1); //dumps stuff out
-        } else if (gamepad1.y) {
-            robot.liftServo(0); //loading position
-        } else if (gamepad1.a) {
-            robot.liftServo(0.4); //middle
+        } else if (gamepad2.x) {
+            robot.liftServo(0); //
+        } else if (gamepad2.a) {
+            robot.liftServo(0.2); //straight up and down
+        } else if (gamepad2.b) {
+            robot.liftServo(0.4); //tilt
         }
 
         //ELEVATOR SERVO
-        if (gamepad1.left_trigger>0.1) {
+        if (gamepad1.y) {
             robot.elevatorServo.setPosition(.7); //horizontal
-        } else if (gamepad1.right_trigger>0.1) {
+        } else if (gamepad1.x) {
             robot.elevatorServo.setPosition(.5); //vertical
+        } else if (gamepad2.dpad_right) {
+            robot.elevatorServo.setPosition(.3); //vertical
         }
 
-        if (gamepad1.dpad_up){
+        //ducky mover
+        if (gamepad2.right_bumper) {
+            double startTime = getRuntime();
+            robot.duckyMover.setPower(-0.4);
+            // if (getRuntime()>=startTime+.25) {
+            //robot.duckyMover.setPower(-.5); //spain without the a in red
+            if (getRuntime() >= startTime + .25) {
+                robot.duckyMover.setPower(-1); //spain without the a in red
+            }
+       //bracket
+    }else if(gamepad2.left_bumper){
+            double startTime = getRuntime();
+            robot.duckyMover.setPower(0.4);
+           // if (getRuntime()>=startTime+.25) {
+            //    robot.duckyMover.setPower(.5); //spain without the a in red
+                if (getRuntime() >= startTime + .25) {
+                    robot.duckyMover.setPower(1); //spain without the a in red
+                }
+            //
+        } else {
+            robot.duckyMover.setPower(0); //freeze
+        }
+
+        //intake motor
+        if (gamepad1.left_trigger>.001) {
+            robot.intakeMotor.setPower(.3); //take it back now y'all
+        } else if (gamepad1.right_trigger>.001){
+            robot.intakeMotor.setPower(-.3); //outoutout
+        } else {
+            robot.intakeMotor.setPower(0);
+        }
+
+        //XRAIL
+        if (gamepad2.dpad_up) {
+            robot.liftMotor.setPower(1);
+        } else if (gamepad2.dpad_down){
+            robot.liftMotor.setPower(-1);
+        } else {
+            robot.liftMotor.setPower(0);
+        }
+
+        //touch sensor
+        if(gamepad1.dpad_up){
+            robot.touchServoLeft.setPosition(robot.LEFT_UP); //
+            robot.touchServoRight.setPosition(robot.RIGHT_UP); //
+
+        } else if (gamepad1.dpad_down) {
+            robot.touchServoLeft.setPosition(robot.LEFT_DOWN); //
+            robot.touchServoRight.setPosition(robot.RIGHT_DOWN); //
+        }
+
+        //preset 3
+    //    if(gamepad1.b){
+     //       robot.presetThree(-1600,-2500,true);
+   //     }
+
+
+        /*if (gamepad1.dpad_up){
             singleButtonPress=true;
             stage=1;
             goal=0;
@@ -89,54 +157,7 @@ public class FreightBotTeleop extends OpMode {
                 stage=0;
                 liftEncoderGoal=-300;
             }
-        }
-
-
-
-        if(gamepad2.a){
-            robot.elevatorServo.setPosition(0.7); //horizontal
-        } else if (gamepad2.b) {
-            robot.elevatorServo.setPosition(0.5); //vertical
-        }
-
-        //ducky mover
-        if(gamepad2.x) {
-            robot.duckyMover.setPower(-0.05); //spain without the a in red
-        }else if(gamepad2.y){
-            robot.duckyMover.setPower(0.05); //spain without the a in blue
-        } else {
-            robot.duckyMover.setPower(0); //freeze
-        }
-
-        //intake motor
-        if (gamepad2.left_bumper) {
-            robot.intakeMotor.setPower(.1); //take it back now y'all
-        } else if (gamepad2.right_bumper){
-            robot.intakeMotor.setPower(-.1); //outoutout
-        } else {
-            robot.intakeMotor.setPower(0);
-        }
-
-        //XRAIL
-        if (gamepad2.dpad_down) {
-            robot.liftMotor.setPower(1); //intake
-        } else if (gamepad2.dpad_up){
-            robot.liftMotor.setPower(-1); //un-intake
-        } else {
-            robot.liftMotor.setPower(0);
-        }
-
-        //touch sensor
-        if(gamepad1.left_bumper){
-            robot.touchServo.setPosition(0); //horizontal
-        } else if (gamepad1.right_bumper) {
-            robot.touchServo.setPosition(0.5); //vertical
-        }
-
-
-
-
-
+        }*/
 
     }
 
